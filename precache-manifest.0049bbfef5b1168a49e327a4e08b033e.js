22c4bd40c61186058accc01f2c5d586a499dca7e
gokul2287 <gokulagiwal2287@gmail.com>
2020-10-23 15:52:15 +0100

    Updates

---
+++ precache-manifest.0049bbfef5b1168a49e327a4e08b033e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a7b9b8825e9acc8f1d7db78417227e",
+    "url": "/covid-react-deploy/index.html"
+  },
+  {
+    "revision": "3be8449a27df1075feaa",
+    "url": "/covid-react-deploy/static/css/2.067ccc96.chunk.css"
+  },
+  {
+    "revision": "be07e36b464cfe1ff900",
+    "url": "/covid-react-deploy/static/css/main.48d98ec1.chunk.css"
+  },
+  {
+    "revision": "3be8449a27df1075feaa",
+    "url": "/covid-react-deploy/static/js/2.91fb1e41.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/covid-react-deploy/static/js/2.91fb1e41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be07e36b464cfe1ff900",
+    "url": "/covid-react-deploy/static/js/main.f8a2e2e3.chunk.js"
+  },
+  {
+    "revision": "351020dd7e9170a52e18",
+    "url": "/covid-react-deploy/static/js/runtime-main.ce8627cb.js"
+  }
+]);
